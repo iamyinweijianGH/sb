57a1ca17458cb1153fcff9176116f28381894ca2
Administrator <Administrator@YWJ>
2017-11-25 15:26:59 +0800

    0

--- V/src/main/java/com/example/demo/mapper/TMapper.java
+++ V/src/main/java/com/example/demo/mapper/TMapper.java
@@ -11,5 +11,12 @@ public interface TMapper {
 	
 	public int addT(T t);
 	
+	/**
+	 * 查询全部
+	 * @param pageNum
+	 * @param pageSize
+	 * @param order
+	 * @return
+	 */
 	public List<T> findAll(Integer pageNum, Integer  pageSize, String order);
 }
